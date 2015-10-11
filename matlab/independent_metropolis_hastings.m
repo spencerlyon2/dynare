3f2e869327121f8da3f435c611fd355c3e93a0c1
Michel Juillard <michel.juillard@mjui.fr>
2015-10-11 18:59:48 +0200

    bayestopt_.jscale is set in dynare_estimation_init.m

--- matlab/independent_metropolis_hastings.m
+++ matlab/independent_metropolis_hastings.m
@@ -90,9 +90,6 @@ else
     if (options_.load_mh_file~=0) && any(fline>1) ,
         NamFileInput(length(NamFileInput)+1,:)={[MetropolisFolder filesesep],[ModelName '_mh' int2str(NewFile(1)) '_blck*.mat']};
     end
-    if exist([ModelName '_optimal_mh_scale_parameter.mat'])
-        NamFileInput(length(NamFileInput)+1,:)={'',[ModelName '_optimal_mh_scale_parameter.mat']};
-    end
     % from where to get back results
     %     NamFileOutput(1,:) = {[M_.dname,'/metropolis/'],'*.*'};
     [fout, nBlockPerCPU, totCPU] = masterParallel(options_.parallel, fblck, nblck,NamFileInput,'independent_metropolis_hastings_core', localVars, globalVars, options_.parallel_info);
